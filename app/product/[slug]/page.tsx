7de4d4e83caaf97256fa95b59d1f5d949699c602
sapiens-dev <sapiens.lien@gmail.com>
2023-11-17 15:48:03 +0700

    change color in [slug]

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -33,17 +33,17 @@ export default async function ProductPge({
   const data: fullProduct = await getData(params.slug);
 
   return (
-    <div className="bg-white">
+    <div className="">
       <div className="mx-auto max-w-screen-xl px-4 md:px-8">
         <div className="grid gap-8 md:grid-cols-2">
           <ImageGallery images={data.images} />
 
           <div className="md:py-8">
             <div className="mb-2 md:mb-3">
-              <span className="mb-0.5 inline-block text-gray-500">
+              <span className="mb-0.5 inline-block ">
                 {data.categoryName}
               </span>
-              <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
+              <h2 className="text-2xl font-bold lg:text-3xl">
                 {data.name}
               </h2>
             </div>
@@ -54,7 +54,7 @@ export default async function ProductPge({
                 <Star className="h-5 w-5" />
               </Button>
 
-              <span className="text-sm text-gray-500 transition duration-100">
+              <span className="text-sm  transition duration-100">
                 56 Ratings
               </span>
             </div>
@@ -69,12 +69,12 @@ export default async function ProductPge({
                 </span>
               </div>
 
-              <span className="text-sm text-gray-500">
+              <span className="text-sm ">
                 Incl. Vat plus shipping
               </span>
             </div>
 
-            <div className="mb-6 flex items-center gap-2 text-gray-500">
+            <div className="mb-6 flex items-center gap-2 ">
               <Truck className="w-6 h-6" />
               <span className="text-sm">2-4 Day Shipping</span>
             </div>
@@ -100,7 +100,7 @@ export default async function ProductPge({
               />
             </div>
 
-            <p className="mt-12 text-base text-gray-500 tracking-wide">
+            <p className="mt-12 text-base  tracking-wide">
               {data.description}
             </p>
           </div>
